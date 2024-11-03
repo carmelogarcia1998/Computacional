a5f06cfe4b1dda23e88ae8e5b82dfea3571b40c7
Carmelo Garcia <19-10252@usb.ve>
2024-11-02 21:53:23 -0400

    Update: Se usa el teorema de Steiner para obtener el momento de inercia del centro de masa.

--- tarea_1/problema_1.c
+++ tarea_1/problema_1.c
@@ -28,7 +28,7 @@ Para el caso \( N = 10^8 \), elabore un programa que evalúe para este sistema:
 #include<math.h>
 
 // Definimos las constantes que vamos a usar.
-int N = 100000000;
+int N = 1e+8;
 float x_0 = 0.3333, y_0 = 0.7453;
 double x_n, y_n, m_n, r_n, x_ant, y_ant, m_t, x_cm=0, y_cm=0, r_cm[2], I=0, x_max, x_min, y_max, y_min, A;
 
@@ -37,13 +37,16 @@ double x_n, y_n, m_n, r_n, x_ant, y_ant, m_t, x_cm=0, y_cm=0, r_cm[2], I=0, x_ma
 void main(){
 x_ant = x_0;
 y_ant = y_0;
-double r_0 = sqrt(x_0*x_0 + y_0*y_0);
-m_t = pow(fabs(x_0*y_0), 1.3)*exp(-r_0/sqrt(2));
-//printf("masa total inicial : %lf\n", m_t);
+//Le sacamos los calculos necesarios a la primera particula
+double r_0 = sqrt(x_0*x_0 + y_0*y_0); //Modulo del vector posicion
+m_t = pow(fabs(x_0*y_0), 1.3)*exp(-r_0/sqrt(2)); // Masa de la particula 0
+x_cm = x_0*m_t; // Aportacion de la particula 0 al centro de masas
+y_cm = y_0*m_t; // Aportacion de la particula 0 al centro de masas
+I = m_t*(x_0*x_0 + y_0*y_0); // Aportacion de la particula 0 al momento de inercia
 // limpiamos la memoria del arreglo r_cm
 for (int i=0; i<1; i++) r_cm[i] = 0;
 
-for (int i=1; i<N; i++){
+for (int i=1; i<=N; i++){
     // componente del vector posicion en x
     x_n = -2.7*x_ant*log(x_ant);
     // componente del vector posicion en y
@@ -58,11 +61,10 @@ for (int i=1; i<N; i++){
     x_cm += x_n*m_n;
     y_cm += y_n*m_n;
 
-    //Calcular el momento de inercia
+    //Calcular el momento de inercia para un eje que pasa por el origen
     I += m_n*(x_n*x_n + y_n*y_n);
 
     //Calcular el area del rectangulo
-
     if (i == 1){
         x_max = x_n;
         x_min = x_n;
@@ -89,10 +91,12 @@ for (int i=1; i<N; i++){
     r_cm[0] = x_cm/m_t;
     r_cm[1] = y_cm/m_t;
 
+    // Calculo del momento de incercia del centro de masa que pasa por el centro de masa usando el teorema de Steiner.
+    double Icm = I - m_t*(r_cm[0]*r_cm[0] + r_cm[1]*r_cm[1]);
+
     printf("La masa total del sistema es: %lf\n", m_t);
     printf("El centro de masas del sistema es r_cm = (%lf, %lf)\n", r_cm[0], r_cm[1]);
-    printf("El momento de inercia del sistema es: %lf\n", I);
-    printf("x_max=%lf ,  x_min=%lf, y_max=%lf, y_min= %lf\n", x_max, x_min, y_max, y_min);
+    printf("El momento de inercia del sistema es: %lf\n", Icm);
     printf("El area del rectangulo es: %lf\n", A);
     
     
