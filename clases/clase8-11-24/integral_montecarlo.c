fa2022f2b8c31c1d367aae2dd7ecca9365f037d4
Carmelo Garcia <19-10252@usb.ve>
2024-11-08 12:47:09 -0400

    integral del seno de 0 a pi usando el metodo de montecarlo

---
+++ clases/clase8-11-24/integral_montecarlo.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
+
+int c=0, r, b, h;
+double xMin, xMax, yMin, yMax, try, tryx, x, y, Arec, Asen;
+
+long double N;
+
+void main(){
+
+        // Declarar una variable para almacenar el tiempo de inicio y fin
+        clock_t start, end; double cpu_time_used; 
+        // Obtener el tiempo de inicio 
+        start = clock();
+
+        srand((long)time((__time_t *)(NULL)));
+
+        xMin=0, xMax=M_PI, yMin=0, yMax=1;
+        N = 1e9;
+
+
+        for (int i=0; i<N; i++){
+
+            tryx = rand()/((double)RAND_MAX);
+            x = xMin + (xMax - xMin)*tryx;
+
+            try = rand()/((double)RAND_MAX);
+            y = yMin + (yMax - yMin)*try;
+
+            //rn = sqrt(x*x + y*y);
+            
+            if (y <= sin(x)) c++;
+        }  
+
+        Asen = (double)(c/N) * M_PI;
+        printf("El valor de pi es: %lf\n", Asen);
+
+         // Obtener el tiempo de fin 
+         end = clock();
+         // Calcular el tiempo de CPU usado
+         cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+         printf("El tiempo de ejecución es: %f segundos\n", cpu_time_used);
+}
